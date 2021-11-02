283a8ff914dfa4faf0f6af5474bb97b05588957f
Jude Mwenda <judemwenda@gmail.com>
2021-11-02 16:00:45 -0400

    Addition of files to git

---
+++ README.md
@@ -0,0 +1,9 @@
+========================
+The application provides a mechanism through which one can enter a starting point and and endpoint background then a traffic and a driving route are plotted. The traffic route is in blue while the plotted driving route is in green.
+
+The application uses the starting location of the browser. If this is not provided the default is utilized.
+The geocoder is also helpful for searching locations for routing.
+
+To select a route, one has to doubleclick the traffic route. Once doubleclicked, an animation occurs and at the end of the trop the transparent popup indicates that the person has arrived. The driving router event has not been activated due to lack of time. I selected the doubleclick as opposed to the click as the click event is used in changing the route direction. So not possible due to a conflict in functions. 
+
+The tilequery API has is only trigged approximately at the halfway point. This is due to lack of time.
---
+++ index.js
@@ -0,0 +1,172 @@
+// ignore this error, it works fine
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam13ZW5kYSIsImEiOiJja3ZndGZrMXRjNHhjMnFxNnlpcWVqZDE0In0.tH-DplQHTRm4ReAGnzEK-w";
+
+
+const map = new mapboxgl.Map({
+  container: "map", // container id
+  //TODO Add a custom style
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  //TODO Localize the map to the user's location
+  center: [-84.3, 33], // starting position [lng, lat]
+  //center: [lng,lat],
+  zoom: 9 // starting zoom
+});
+
+var traffic =   new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+    alternatives: false,
+    profile: 'mapbox/driving-traffic',
+    controls: {instructions: false, profileSwitcher: false},
+    geometries: 'geojson',
+    flyTo: true});
+
+window.traffic = traffic;
+//const steps = 500;
+// create a function to make a directions request
+async function getRoute(start,stop) {
+  // make directions request using driving profile
+  begin = start.geometry.coordinates;
+  end = stop.geometry.coordinates
+  const query = await fetch(
+     `https://api.mapbox.com/directions/v5/mapbox/driving/${begin[0]},${begin[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+     { method: 'GET' }
+   );
+   const json = await query.json();
+   const data = json.routes[0];
+   const route = data.geometry.coordinates;
+   const geojson = {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'LineString',
+            'coordinates': route
+          }
+        };
+
+   if (map.getSource('drivroute')) {
+     map.getSource('drivroute').setData(geojson);
+   }else {
+          map.addLayer({
+            'id': 'drivroute',
+            'type': 'line',
+            'source': {
+              'type': 'geojson',
+              'data': geojson
+            },
+            'layout': {
+              'line-join': 'round',
+              'line-cap': 'round'
+            },
+            'paint': {
+              'line-color': 'green',
+              'line-width': 5,
+              'line-opacity': 0.75
+            }
+          });
+        }
+  }
+async function poiQuery(point) {
+  //console.log(point)
+  const query = await fetch(
+     `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/${point[0]},${point[1]}.json?radius=50&limit=5&dedupe&geometry=point&access_token=${mapboxgl.accessToken}`,
+     { method: 'GET' }
+   );
+   const json = await query.json();
+   //console.log(json);
+   document.getElementById('features').innerHTML = JSON.stringify(json);
+}
+
+map.addControl(traffic, 'top-left');
+
+// Add geolocate control to the map.
+map.addControl(
+  new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true
+  },
+  // When active the map will receive updates to the device's location as it changes.
+  trackUserLocation: true,
+  // Draw an arrow next to the location dot to indicate which direction the device is heading.
+  showUserHeading: true
+  })
+);
+
+let counter = 0;
+const steps = 1000;
+
+map.on("load", () => {
+  thesource = [0,0];
+  thedestination = [];
+
+  //we zppm to the place if there are crdinates
+  if (navigator.geolocation) {
+    thesource = []
+    navigator.geolocation.getCurrentPosition(function(position) {
+      coordinates = [position.coords.longitude , position.coords.latitude];
+      thesource = coordinates;
+      map.flyTo({center: coordinates, essential: true});
+      traffic.setOrigin(thesource);
+    });
+  }
+
+  const point = {
+    'type': 'FeatureCollection',
+    'features': [{
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'Point',
+        'coordinates': thesource
+      }
+    }
+  ]};
+  map.addSource('point', {
+    'type': 'geojson',
+    'data': point
+  });
+  map.addLayer({
+      'id': 'point',
+      'source': 'point',
+      'type': 'symbol',
+      'layout': {
+          'icon-image': 'car-15',
+          'icon-rotate': ['get', 'bearing'],
+          'icon-rotation-alignment': 'map',
+          'icon-allow-overlap': true,
+          'icon-ignore-placement': true
+      }
+  });
+
+  traffic.on('route',function(d)
+  {
+    getRoute(traffic.getOrigin(),traffic.getDestination());
+  });
+  map.on('dblclick', 'directions-route-line' , function (e) {
+    animate(e.features[0].geometry.coordinates);
+   });
+  function animate(){
+     thelength = map.getSource('directions')._data.features.length;
+     navroute = map.getSource('directions')._data.features[thelength-1].geometry;
+     start = navroute.coordinates[counter];
+     end = navroute.coordinates[counter+1];
+     if (!start || !end){
+       document.getElementById('features').innerHTML = "You've arrived";
+       return
+     };
+     point.features[0].geometry.coordinates = navroute.coordinates[counter];
+     point.features[0].properties.bearing = turf.bearing(
+       turf.point(start),
+       turf.point(end));
+     map.getSource('point').setData(point);
+     //doing this to get at center point due to no time
+     if (counter == Math.round(navroute.coordinates.length/ 2)){
+       poiQuery(end);
+     }
+
+     if (counter < steps) {
+     requestAnimationFrame(animate);
+    }
+     counter = counter + 1;
+   }
+});
